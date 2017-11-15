bea30ad80727cd4b9d4ffabe5c75b015309484d7
urbannator <murban2@dtcc.edu>
2017-11-14 19:06:31 -0500

    previous and next month

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit, Input } from '@angular/core';
     styleUrls: [ 'calendar.component.css' ]
 })
 export class CalendarComponent implements OnInit {
+
+    whatmonth: number = 1;
+
+
     constructor() {
+    }
+
+    getcurrentmonth(){
+      return this.whatmonth;
+    }
+
+    previousmonth(){
+      if(this.whatmonth == 1){
+        this.whatmonth = 12;
+      }
+      else{
+        this.whatmonth = this.whatmonth - 1;
+      }
+    }
+
+    nextmonth(){
+      if(this.whatmonth == 12){
+        this.whatmonth = 1;
+      }
+      else{
+        this.whatmonth = this.whatmonth + 1;
+      }
     }
 
     ngOnInit(): void {
