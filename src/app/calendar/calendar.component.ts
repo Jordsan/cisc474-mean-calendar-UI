6d58c3edbed64e3e9f23563ed2516b8137069e7e
urbannator <murban2@dtcc.edu>
2017-11-16 16:22:57 -0500

    When opening 'app' starting date is now whatever the current month is

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -10,15 +10,37 @@ export class CalendarComponent implements OnInit {
     currentMonth: number = 2;
     currentYear: number = 2017;
     daysInMonth: number;
+    startingYearCalendar: number = 1;
+    startingMonthCalendar: number = 1;
 
     constructor() {
     }
 
     getCurrentYear(){
+      // Return today's date and time
+      var currentTime = new Date()
+      // returns the month (from 0 to 11)
+      var month = currentTime.getMonth() + 1
+      // returns the day of the month (from 1 to 31)
+      var day = currentTime.getDate()
+      // returns the year (four digits)
+      var year = currentTime.getFullYear()
+      if(this.startingYearCalendar==1){
+        this.currentYear=year;
+        this.startingYearCalendar=0;
+      }
       return this.currentYear
     }
 
     getCurrentMonth(){
+      // Return today's date and time
+      var currentTime = new Date()
+      // returns the month (from 0 to 11)
+      var month = currentTime.getMonth() + 1
+      if(this.startingMonthCalendar==1){
+        this.currentMonth=month;
+        this.startingMonthCalendar=0;
+      }
       return this.currentMonth;
     }
 
